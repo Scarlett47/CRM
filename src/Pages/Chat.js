810451b59f52a88ccf3b6bc7f2910ec1db4c2ae8
Kristina094 <102580856+Kristina094@users.noreply.github.com>
2023-02-04 14:14:10 +0800

    dfd

---
+++ src/Pages/Chat.js
@@ -0,0 +1,55 @@
+import React from 'react'
+
+export default function Chat() {
+  return (
+    <div className="smallBox">
+						<div style={{ display: "flex", justifyContent: "space-between" }}>
+							<Title style={{ margin: 0, paddingLeft: 10 }} level={4}>
+								Send benefit review by Sunday
+							</Title>
+							<Title
+								style={{
+									margin: 0,
+									paddingRight: 10,
+									color: "#90A0B7",
+								}}
+								level={5}
+							>
+								Reminder
+							</Title>
+						</div>
+						<Title
+							style={{
+								margin: 0,
+								color: "#90A0B7",
+								paddingLeft: 10,
+							}}
+							level={5}
+						>
+							Due to: <span style={{ color: "black" }}>December 23, 2018</span>
+						</Title>
+						<br />
+						<br />
+						<div style={{ display: "flex", justifyContent: "space-between" }}>
+							<div>
+								<Avatar
+									size={32}
+									style={{
+										marginLeft: 10,
+										float: "left",
+									}}
+								/>
+								<Title
+									level={5}
+									style={{ margin: 0, paddingLeft: 5, float: "left" }}
+								>
+									George Fields
+								</Title>
+							</div>
+							<Button type="primary" style={{ marginRight: 10 }}>
+								Completed
+							</Button>
+						</div>
+					</div>
+  )
+}
